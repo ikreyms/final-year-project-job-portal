5ed93042b0afe2d7042ee14c3e8ab76506177615
Mohamed Ikram <ikreyms@gmail.com>
2022-02-23 19:00:01 +0500

    basic express boilerplate added

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("job-portal-final-year-projectserver"));
+
+app.listen(process.env.PORT || 2900, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
