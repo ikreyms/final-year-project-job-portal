f8360e22c67eb286952e8775f67f8932b9d0f52d
Mohamed Ikram <ikreyms@gmail.com>
2022-03-01 17:33:10 +0500

    added new function checkIfLoggedIn which is called on mount

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Homepage from "./components/Homepage";
@@ -9,9 +9,42 @@ import Employer from "./components/Employer";
 import Auth from "./components/Auth";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useTheme } from "@mui/styles";
+import axios from "axios";
+import { login } from "./redux/actionCreators";
+import { useDispatch } from "react-redux";
 
 const App = () => {
   console.log(useTheme());
+
+  const dispatch = useDispatch();
+
+  const checkIfLoggedIn = async () => {
+    const token = localStorage.getItem("joblookupLoginToken");
+    try {
+      const response = await axios.post(
+        "http://localhost:2900/api/auth",
+        null,
+        {
+          headers: {
+            contentType: "application/json",
+            authorization: token,
+          },
+        }
+      );
+      dispatch(login(response.data.user));
+      console.log(response.data.user);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response);
+      } else {
+        console.log("server error");
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkIfLoggedIn();
+  }, []);
 
   return (
     <Router>
