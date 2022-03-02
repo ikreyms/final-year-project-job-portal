8c2200720c7201e50ff91e400af71f359ff966dc
Mohamed Ikram <ikreyms@gmail.com>
2022-03-02 16:20:36 +0500

    Commented out some imports and routes to ensure no errors during the update of models

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Homepage from "./components/Homepage";
 import Jobs from "./components/Jobs";
 import About from "./components/About";
 import Employers from "./components/Employers";
-import Employer from "./components/Employer";
+// import Employer from "./components/Employer";
 import Auth from "./components/Auth";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useTheme } from "@mui/styles";
@@ -53,7 +53,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="jobs" element={<Jobs />} />
         <Route path="employers" element={<Employers />} />
-        <Route path="employers/:id" element={<Employer />} />
+        {/* <Route path="employers/:id" element={<Employer />} /> */}
 
         {/* if user manually enters a route, and it is invalid:  */}
         {/* <Route path="*" element={<Employers />} />  <=not working... learn more... */}
