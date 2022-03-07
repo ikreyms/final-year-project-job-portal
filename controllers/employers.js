0047509442551f66c5ab06bdedf0a0e8f8763ac6
Mohamed Ikram <ikreyms@gmail.com>
2022-03-08 03:41:39 +0500

    added filterEmployers function

--- controllers/employers.js
+++ controllers/employers.js
@@ -3,4 +3,15 @@ const responseToClient = require("../utils/responseToClient");
 
 exports.filterEmployers = async (req, res, next) => {
   let { sort, order, limit, page } = req.query;
+  try {
+    const employers = await Employer.find()
+      .sort({ [sort]: order })
+      .limit(limit)
+      .populate("openings");
+    res.json({ success: true, employers });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
 };
+
+//"http://localhost:4000/employers?_sort=rating&_order=desc&_limit=6" + [page]
