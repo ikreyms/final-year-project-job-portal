3b99731bf665e5e1bdc93d7c618e383373f4f542
Mohamed Ikram <ikreyms@gmail.com>
2022-03-07 18:09:55 +0500

    added new route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const authRoutes = require("./routes/auth");
+const employerRoutes = require("./routes/employers");
 const connectDB = require("./config/db");
 
 connectDB();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("job-portal-final-year-project/server"));
 app.use("/api/auth", authRoutes);
+app.use("/api/employers", employerRoutes);
 
 app.listen(process.env.PORT || 2900, () =>
   console.log(`Server running on port ${process.env.PORT}`)
