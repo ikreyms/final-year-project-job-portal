ba7a298b8bc5e114502c8d7687445cfcc003a763
Mohamed Ikram <ikreyms@gmail.com>
2022-03-08 18:16:26 +0500

    finished the filterEmployers function

--- controllers/employers.js
+++ controllers/employers.js
@@ -3,14 +3,48 @@ const responseToClient = require("../utils/responseToClient");
 
 exports.filterEmployers = async (req, res, next) => {
   let { sort, order, limit, page } = req.query;
+  let skip;
+  let query;
+  let dbFieldObject = {};
+  Object.keys(req.query).filter((key) => {
+    if (
+      key !== "sort" &&
+      key !== "order" &&
+      key !== "limit" &&
+      key !== "page"
+    ) {
+      dbFieldObject[[key]] = req.query[key];
+      return true;
+    }
+  });
+
   try {
-    const employers = await Employer.find()
-      .sort({ [sort]: order })
-      .limit(limit)
-      .populate("openings");
-    res.json({ success: true, employers });
+    const count = await Employer.estimatedDocumentCount();
+
+    if (!page) {
+      query = await Employer.find(dbFieldObject)
+        .sort({ [sort]: order })
+        .limit(limit)
+        .populate("openings")
+        .populate("jobsPosted")
+        .populate("followers");
+    } else {
+      if (limit === undefined) limit = 10;
+      skip = 10 * page - 10;
+      query = await Employer.find(dbFieldObject)
+        .sort({ [sort]: order })
+        .populate("openings")
+        .populate("jobsPosted")
+        .populate("followers")
+        .skip(skip)
+        .limit(limit);
+    }
+    res
+      .set("total-doc-count", count)
+      .status(200)
+      .json({ success: true, employers: query });
   } catch (error) {
-    res.json({ error: error.message });
+    responseToClient(res, 400, { error: error.message });
   }
 };
 
