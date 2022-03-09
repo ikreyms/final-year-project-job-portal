2a5c54232da2fe9c4baba3195bf910f59cf7d9c1
Mohamed Ikram <ikreyms@gmail.com>
2022-03-09 20:12:00 +0500

    added userRoutes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const express = require("express");
 const authRoutes = require("./routes/auth");
 const employerRoutes = require("./routes/employers");
+const userRoutes = require("./routes/users");
 const connectDB = require("./config/db");
 
 connectDB();
@@ -20,6 +21,7 @@ app.use(express.json());
 app.get("/", (req, res) => res.send("job-portal-final-year-project/server"));
 app.use("/api/auth", authRoutes);
 app.use("/api/employers", employerRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT || 2900, () =>
   console.log(`Server running on port ${process.env.PORT}`)
