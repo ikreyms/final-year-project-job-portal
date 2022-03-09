848afd18f905d7d750ef5e8df3eec250b5364158
Mohamed Ikram <ikreyms@gmail.com>
2022-03-09 20:09:24 +0500

    no major changes

--- routes/employers.js
+++ routes/employers.js
@@ -3,11 +3,13 @@ const express = require("express");
 const {
   filterEmployers,
   searchEmployers,
+  getOneEmployer,
 } = require("../controllers/employers");
 
 const router = express.Router();
 
 router.get("/", filterEmployers);
 router.get("/search", searchEmployers);
+router.get("/:id", getOneEmployer);
 
 module.exports = router;
