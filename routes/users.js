33302ca809becc93fa2bb38628413ca9db128f43
Mohamed Ikram <ikreyms@gmail.com>
2022-03-13 03:18:02 +0500

    added three new routes: getUserRatingsAndFollowing, followEmplofollowEmployer and UnfollowEmployer

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
-const { getOneUser } = require("../controllers/users");
+const {
+  getOneUser,
+  getUserRatingsAndFollowing,
+  followNewEmployer,
+  unfollowNewEmployer,
+} = require("../controllers/users");
 
 const router = express.Router();
 
 router.get("/:id", getOneUser);
+router.get("/:id/getUserRatingsAndFollowing", getUserRatingsAndFollowing);
+router.patch("/follow/:userId/:employerId", followNewEmployer);
+router.patch("/unfollow/:userId/:employerId", unfollowNewEmployer);
 
 module.exports = router;
