c370dfcb83802ac162f6de51af8514ee475f2cd7
Mohamed Ikram <ikreyms@gmail.com>
2022-03-14 18:11:58 +0500

    turned on timestamps and added duedata as index, now when due date is up document deletes automatically

--- models/Job.js
+++ models/Job.js
@@ -1,27 +1,28 @@
 const mongoose = require("mongoose");
 const Employer = require("./Employer");
 
-const jobSchema = new mongoose.Schema({
-  postedBy: {
-    required: true,
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Employer",
+const jobSchema = new mongoose.Schema(
+  {
+    postedBy: {
+      required: true,
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Employer",
+    },
+
+    title: {
+      required: [true, "Please provide the title of the job."],
+      type: String,
+    },
+
+    description: String,
+
+    dueDate: { type: Date },
+
+    //   "postDate": "2022-02-12T01:12:59"
   },
+  { timestamps: true, timestamps: { createdAt: "postDate" } }
+);
 
-  title: {
-    required: [true, "Please provide the title of the job."],
-    type: String,
-  },
-
-  postDate: Date,
-
-  dueDate: Date,
-
-  description: String,
-
-  //   "postDate": "2022-02-12T01:12:59",
-
-  //   "dueDate": "2022-02-25T23:59:59"
-});
+jobSchema.index({ dueDate: 1 }, { expireAfterSeconds: 0 });
 
 module.exports = new mongoose.model("Job", jobSchema);
