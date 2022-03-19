2bd1bfecb6e3e1603187c728ded0965f9d25703a
Mohamed Ikram <ikreyms@gmail.com>
2022-03-20 02:58:15 +0500

    created file for storing const data arrays for UI

---
+++ client/src/assets/dataArrays.js
@@ -0,0 +1,27 @@
+exports.jobTypes = ["All", "Part-time", "Full-time", "Intern"];
+
+exports.salaryRanges = [
+  "All",
+  "1000-2999",
+  "3000-4999",
+  "5000-6999",
+  "7000-9999",
+  "10000-14999",
+  "15000-19999",
+  "20000+",
+];
+
+exports.jobCategories = [
+  "All",
+  "Agriculture, Food, and Natural Resources",
+  "Architecture and Construction",
+  "Arts, Audio/Video Technology, and Communication",
+  "Business and Finance",
+  "Education and Training",
+  "Government and Public Administration",
+  "Health Science",
+  "Information Technology",
+  "Law, Public Safety, Corrections, and Security",
+  "Marketing",
+  "Science, Technology, Engineering, and Math",
+];
