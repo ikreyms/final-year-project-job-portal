fbaf9ea9baa647d7dde88514f668d9c204e33f88
Mohamed Ikram <ikreyms@gmail.com>
2022-03-20 16:51:23 +0500

    created JobResult component

---
+++ client/src/components/Jobs/JobResult.js
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import moment from "moment";
+import logo from "../../assets/logo.svg";
+import useStyles from "./styles";
+
+const JobResult = ({ job }) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.jobResult} elevation={1}>
+      <CardActionArea
+        sx={{ display: "flex", justifyContent: "flex-start", px: 2 }}
+      >
+        <CardMedia
+          component="img"
+          image={job.postedBy.image ? job.postedBy.image : logo}
+          height="40"
+          sx={{ maxWidth: 40, borderRadius: 1, cursor: "pointer" }}
+          alt="employer logo"
+        />
+        <CardContent
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography variant="subtitle2">{job.title}</Typography>
+            <Typography variant="caption">
+              {job.postedBy.companyName}
+              <br />
+            </Typography>
+            <Typography variant="caption">MVR {job.salary}</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+            }}
+          >
+            <Typography variant="caption">
+              Posted: {moment(job.postDate).fromNow()}
+            </Typography>
+            <Typography variant="caption">
+              Due on: {moment(job.dueDate).format("DD/MM/YYYY")}
+            </Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default JobResult;
