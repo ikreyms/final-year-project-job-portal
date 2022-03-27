c668de4db52728d35421e651efb9ce37c8383074
Mohamed Ikram <ikreyms@gmail.com>
2022-03-27 19:47:41 +0500

    added new styles

--- client/src/components/Job/styles.js
+++ client/src/components/Job/styles.js
@@ -4,6 +4,36 @@ const useStyles = makeStyles((theme) => ({
   container: {
     ...theme.presetStyles.containerStyle,
   },
+  main: {
+    display: "flex",
+    marginTop: `${theme.spacing(2)} !important`,
+    marginBottom: `${theme.spacing(3)} !important`,
+    [theme.breakpoints.down("xs")]: { flexDirection: "column" },
+  },
+  actionArea: {
+    width: "45%",
+    padding: theme.spacing(3),
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+      paddingRight: 0,
+      paddingLeft: 0,
+    },
+  },
+  jobDisplay: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    gap: 1,
+  },
+  btnSet: {
+    display: "flex",
+    gap: 2,
+    flexWrap: "wrap",
+  },
+  btn: {
+    flexGrow: 1,
+    [theme.breakpoints.down("xs")]: { flexBasis: 1 },
+  },
 }));
 
 export default useStyles;
