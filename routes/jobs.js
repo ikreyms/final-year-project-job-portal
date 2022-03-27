27a8ada2a8d7231d561069c155cebaf2539973f2
Mohamed Ikram <ikreyms@gmail.com>
2022-03-27 19:44:00 +0500

    added getOneJob and getSimilarJobs route

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,17 @@
 const express = require("express");
 
-const { createJob, filterJobs } = require("../controllers/jobs");
+const {
+  createJob,
+  filterJobs,
+  getOneJob,
+  getSimilarJobs,
+} = require("../controllers/jobs");
 
 const router = express.Router();
 
 router.post("/:employerId/create", createJob);
 router.get("/", filterJobs);
+router.get("/:id", getOneJob);
+router.get("/getSimilar/:jobId", getSimilarJobs);
 
 module.exports = router;
