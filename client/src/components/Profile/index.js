b13194a0adbc22049d15e39ebefd4da18ddc0cb6
Mohamed Ikram <ikreyms@gmail.com>
2022-04-07 22:38:15 +0500

    added tabs and tabPanels for jobseeker employer and admin

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Tabs, Tab, Typography, Box } from "@mui/material";
 import { useSelector } from "react-redux";
 import useStyles from "./styles";
 import TabPanel from "./TabPanel";
-import Dashboard from "./Dashboard";
+import DashboardJobSeeker from "./DashboardJobSeeker";
 import {
   employerProfileTabs,
   jobSeekerProfileTabs,
   adminProfileTabs,
 } from "../../assets/dataArrays";
+import DashboardEmployer from "./DashboardEmployer";
 
 const Profile = () => {
   const classes = useStyles();
+
+  const resumeRef = useRef();
+  const brandingRef = useRef();
+
   const accountType = useSelector((state) => state.user?.accountType);
+  const profileId = useSelector((state) => state.user?.id);
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
@@ -23,38 +29,95 @@ const Profile = () => {
     <Box className={classes.section}>
       <Box className={classes.container}>
         <Box className={classes.main}>
-          <Tabs
-            orientation="vertical"
-            variant="scrollable"
-            value={value}
-            onChange={handleChange}
-            sx={{ borderRight: 1, borderColor: "divider", width: 180 }}
-          >
-            {accountType === "Job Seeker" ? (
-              jobSeekerProfileTabs.map((item) => <Tab label={item} />)
-            ) : accountType === "Employer" ? (
-              employerProfileTabs.map((item) => <Tab label={item} />)
-            ) : accountType === "Admin" ? (
-              adminProfileTabs.map((item) => <Tab label={item} />)
-            ) : (
-              <Tab label="Please Login" />
-            )}
-          </Tabs>
-          <TabPanel value={value} index={0}>
-            <Dashboard />
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            Branding
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            Jobs
-          </TabPanel>
-          <TabPanel value={value} index={3}>
-            Applications
-          </TabPanel>
-          <TabPanel value={value} index={4}>
-            Interviews
-          </TabPanel>
+          {accountType === "Job Seeker" ? (
+            <>
+              <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                sx={{ borderRight: 1, borderColor: "divider", width: 180 }}
+              >
+                {jobSeekerProfileTabs.map((item, index) => {
+                  if (index === 1) {
+                    return <Tab key={index} label={item} ref={resumeRef} />;
+                  }
+                  return <Tab key={index} label={item} />;
+                })}
+              </Tabs>
+              <TabPanel value={value} index={0}>
+                <DashboardJobSeeker resumeRef={resumeRef} />
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                Resume
+              </TabPanel>
+              <TabPanel value={value} index={2}>
+                Applied Jobs
+              </TabPanel>
+              <TabPanel value={value} index={3}>
+                Notifications
+              </TabPanel>
+            </>
+          ) : accountType === "Employer" ? (
+            <>
+              <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                sx={{ borderRight: 1, borderColor: "divider", width: 180 }}
+              >
+                {employerProfileTabs.map((item, index) => {
+                  if (index === 1)
+                    return <Tab key={index} label={item} ref={brandingRef} />;
+                  return <Tab key={index} label={item} />;
+                })}
+              </Tabs>
+              <TabPanel value={value} index={0}>
+                <DashboardEmployer brandingRef={brandingRef} />
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                Branding
+              </TabPanel>
+              <TabPanel value={value} index={2}>
+                Jobs
+              </TabPanel>
+              <TabPanel value={value} index={3}>
+                Applications
+              </TabPanel>
+              <TabPanel value={value} index={4}>
+                Interviews
+              </TabPanel>
+            </>
+          ) : accountType === "Admin" ? (
+            <>
+              <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                sx={{ borderRight: 1, borderColor: "divider", width: 180 }}
+              >
+                {adminProfileTabs.map((item, index) => (
+                  <Tab key={index} label={item} />
+                ))}
+              </Tabs>
+              <TabPanel value={value} index={0}>
+                <DashboardJobSeeker resumeRef={resumeRef} />
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                Resume
+              </TabPanel>
+              <TabPanel value={value} index={2}>
+                Applied Jobs
+              </TabPanel>
+              <TabPanel value={value} index={3}>
+                Notifications
+              </TabPanel>
+            </>
+          ) : (
+            <>{<Tab label="Please Login" />}</>
+          )}
         </Box>
       </Box>
     </Box>
