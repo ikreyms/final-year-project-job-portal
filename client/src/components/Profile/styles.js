15a2d55cdd8f85fbb9edf7cb1f043da3bba0ec4c
Mohamed Ikram <ikreyms@gmail.com>
2022-04-07 22:36:40 +0500

    added header styles for dashboard

--- client/src/components/Profile/styles.js
+++ client/src/components/Profile/styles.js
@@ -11,6 +11,47 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     margin: `${theme.spacing(4)} 0 ${theme.spacing(4)}`,
   },
+
+  //dashboard
+  header: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerMain: {
+    display: "flex",
+    alignItems: "center",
+  },
+  headerMinor: {
+    marginRight: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  profilePic: {
+    height: 100,
+    cursor: "pointer !important",
+  },
+  headerMainTextWrapper: { marginLeft: theme.spacing(3) },
+  statPacks: {
+    display: "flex",
+    justifyContent: "space-evenly",
+  },
+  statPack: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  statCircle: {
+    color: theme.palette.primary.dark,
+    height: 100,
+    width: 100,
+    borderRadius: "50%",
+    boxShadow: theme.shadows[4],
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 }));
 
 export default useStyles;
