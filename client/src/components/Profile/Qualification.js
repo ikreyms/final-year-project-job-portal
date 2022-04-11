d8bd5c69fa1f87b4fd6bd6dcbeab829f8ef0b301
Mohamed Ikram <ikreyms@gmail.com>
2022-04-11 22:41:25 +0500

    handleChange function created to update state of parent component

--- client/src/components/Profile/Qualification.js
+++ client/src/components/Profile/Qualification.js
@@ -2,8 +2,20 @@ import { Grid, TextField, Typography } from "@mui/material";
 import React from "react";
 import useStyles from "./styles";
 
-const Qualification = ({ no }) => {
+const Qualification = ({ no, qualifications, setQualifications }) => {
   const classes = useStyles();
+
+  const handleChange = (e) => {
+    setQualifications((prev) =>
+      prev.map((obj, index) => {
+        if (index === no - 1) {
+          return { ...obj, [e.target.name]: e.target.value };
+        }
+        return obj;
+      })
+    );
+  };
+
   return (
     <>
       <Typography mt={2} mb={1}>
@@ -22,6 +34,9 @@ const Qualification = ({ no }) => {
             className={classes.formControl}
             label="Institute"
             margin="dense"
+            name="institute"
+            value={qualifications[no - 1].institute}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item z={2} xs={1}>
@@ -31,6 +46,9 @@ const Qualification = ({ no }) => {
             className={classes.formControl}
             label="Year"
             margin="dense"
+            name="completedOn"
+            value={qualifications[no - 1].completedOn}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item z={2} xs={1}>
@@ -40,6 +58,9 @@ const Qualification = ({ no }) => {
             className={classes.formControl}
             label="Level"
             margin="dense"
+            name="level"
+            value={qualifications[no - 1].level}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item z={2}>
@@ -49,6 +70,9 @@ const Qualification = ({ no }) => {
             className={classes.formControl}
             label="Course"
             margin="dense"
+            name="courseName"
+            value={qualifications[no - 1].courseName}
+            onChange={handleChange}
           />
         </Grid>
       </Grid>
