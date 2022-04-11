0b256c2ad2f6ac3a758cafe255da1f54cad4b677
Mohamed Ikram <ikreyms@gmail.com>
2022-04-11 22:42:35 +0500

    handleChange function created to update state of parent component

--- client/src/components/Profile/Experience.js
+++ client/src/components/Profile/Experience.js
@@ -2,8 +2,20 @@ import { Grid, TextField, Typography } from "@mui/material";
 import React from "react";
 import useStyles from "./styles";
 
-const Experience = ({ no }) => {
+const Experience = ({ no, experiences, setExperiences }) => {
   const classes = useStyles();
+
+  const handleChange = (e) => {
+    setExperiences((prev) =>
+      prev.map((obj, index) => {
+        if (index === no - 1) {
+          return { ...obj, [e.target.name]: e.target.value };
+        }
+        return obj;
+      })
+    );
+  };
+
   return (
     <>
       <Typography mt={2} mb={1}>
@@ -22,6 +34,9 @@ const Experience = ({ no }) => {
             className={classes.formControl}
             label="Employer"
             margin="dense"
+            name="employer"
+            value={experiences[no - 1].employer}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item z={2}>
@@ -31,6 +46,9 @@ const Experience = ({ no }) => {
             className={classes.formControl}
             label="Job Title"
             margin="dense"
+            name="jobTitle"
+            value={experiences[no - 1].jobTitle}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item z={1} xs={1}>
@@ -40,6 +58,9 @@ const Experience = ({ no }) => {
             className={classes.formControl}
             label="From"
             margin="dense"
+            name="from"
+            value={experiences[no - 1].from}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item z={1} xs={1}>
@@ -49,6 +70,9 @@ const Experience = ({ no }) => {
             className={classes.formControl}
             label="To"
             margin="dense"
+            name="to"
+            value={experiences[no - 1].to}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item z={1} xs={1}>
@@ -58,6 +82,9 @@ const Experience = ({ no }) => {
             className={classes.formControl}
             label="Duration"
             margin="dense"
+            name="duration"
+            value={experiences[no - 1].duration}
+            onChange={handleChange}
           />
         </Grid>
         <Grid item z={2}>
@@ -67,6 +94,9 @@ const Experience = ({ no }) => {
             className={classes.formControl}
             label="Job Category"
             margin="dense"
+            name="category"
+            value={experiences[no - 1].category}
+            onChange={handleChange}
           />
         </Grid>
       </Grid>
