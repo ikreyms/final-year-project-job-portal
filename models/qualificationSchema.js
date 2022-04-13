79f068c9c0d3b10f1011bc59fe159a6225b58ed6
Mohamed Ikram <ikreyms@gmail.com>
2022-04-13 22:50:38 +0500

    added error messages if required fields not provided

--- models/qualificationSchema.js
+++ models/qualificationSchema.js
@@ -4,19 +4,19 @@ const qualificationSchema = new mongoose.Schema(
   {
     institute: {
       type: String,
-      required: true,
+      required: [true, "Please provide the name of the educational institute."],
     },
     completedOn: {
       type: Date,
-      required: true,
+      required: [true, "Please provide the year of completion."],
     },
     level: {
       type: String,
-      required: true,
+      required: [true, "Please provide the level of course."],
     },
     courseName: {
       type: String,
-      required: true,
+      required: [true, "Please provide the course name."],
     },
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
