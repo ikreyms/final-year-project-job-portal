cc42193a7c490f2a9cc25624015809231713da30
Mohamed Ikram <ikreyms@gmail.com>
2022-04-13 22:50:51 +0500

    added error messages if required fields not provided

--- models/experienceSchema.js
+++ models/experienceSchema.js
@@ -18,23 +18,23 @@ const experienceSchema = new mongoose.Schema(
   {
     employer: {
       type: String,
-      required: true,
+      required: [true, "Please provide the employer name."],
     },
     jobTitle: {
       type: String,
-      required: true,
+      required: [true, "Please provide the title of the job."],
     },
     duration: {
       type: Number,
-      required: true,
+      required: [true, "Please provide the duration of your employment."],
     },
     from: {
       type: Date,
-      required: true,
+      required: [true, "Provide the employment start year."],
     },
     to: {
       type: Date,
-      required: true,
+      required: [true, "Provide the employment termination year."],
     },
     category: {
       required: [true, "Please select a job category that matches your job."],
