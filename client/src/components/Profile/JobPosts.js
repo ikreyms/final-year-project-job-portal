7c86087f95a5b69ca85019a9580d5d83be8fb466
Mohamed Ikram <ikreyms@gmail.com>
2022-04-15 22:22:32 +0500

    created

---
+++ client/src/components/Profile/JobPosts.js
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+import useStyles from "./styles";
+
+const JobPosts = () => {
+  const classes = useStyles();
+  return <Box className={classes.panelWrapper}>h</Box>;
+};
+
+export default JobPosts;
