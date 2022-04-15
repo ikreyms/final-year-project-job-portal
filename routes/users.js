c691a290009a51391a93296834126f9e850a2784
Mohamed Ikram <ikreyms@gmail.com>
2022-04-15 17:45:09 +0500

    removed the removeQualification route

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@ const {
   unfollowNewEmployer,
   rateEmployer,
   updateResume,
-  removeQualification,
 } = require("../controllers/users");
 
 const router = express.Router();
@@ -18,6 +17,5 @@ router.patch("/follow/:userId/:employerId", followNewEmployer);
 router.patch("/unfollow/:userId/:employerId", unfollowNewEmployer);
 router.patch("/rate/:userId/:employerId/:value", rateEmployer);
 router.patch("/resume/:userId", updateResume);
-router.patch("resume/remove/qualification/:userId", removeQualification);
 
 module.exports = router;
