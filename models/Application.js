daf4ea542fccc6ba3abd5c439e5b5e99f5f34b3f
Mohamed Ikram <ikreyms@gmail.com>
2022-04-17 21:17:06 +0500

    finalized fields: seekerId, jobId

--- models/Application.js
+++ models/Application.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
-const applicationSchema = new mongoose.Schema({
-  empId: {
-    required: true,
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Employer",
+const applicationSchema = new mongoose.Schema(
+  {
+    seekerId: {
+      required: true,
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "User",
+    },
+    jobId: {
+      required: true,
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Job",
+    },
   },
-  seekerId: {
-    required: true,
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
 module.exports = new mongoose.model("Application", applicationSchema);
