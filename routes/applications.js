fd5d9d31873612b910c8d2879b802f70b8033c9b
Mohamed Ikram <ikreyms@gmail.com>
2022-05-12 18:31:36 +0500

    added routes: rejectApplication, acceptApplication, filterApplication

--- routes/applications.js
+++ routes/applications.js
@@ -8,19 +8,27 @@ const {
   getApplicationsCountBySeeker,
   getApplicationsReceivedCount,
   getApplicationsReceived,
+  rejectApplication,
+  acceptApplication,
+  filterApplications,
 } = require("../controllers/applications");
 
 const router = express.Router();
 
 router.post("/:seekerId/:jobId/:empId/create", createApplication);
 
-router.get(":seekerId/count/seeker", getApplicationsCountBySeeker);
+router.get("/:seekerId/count/seeker", getApplicationsCountBySeeker);
 router.get("/:seekerId/seeker", getApplicationsBySeeker);
 
-router.get("employer/count/:empId", getApplicationsReceivedCount);
+router.get("/employer/count/:empId", getApplicationsReceivedCount);
 router.get("/:empId/employer", getApplicationsReceived);
 
-router.patch("/:appId/:seekerId", hideApplication);
+router.patch("/seeker/:appId/:seekerId/hide", hideApplication);
 router.patch("/:seekerId", unhideAllApplications);
+
+router.patch("/rejectApplication/:appId", rejectApplication);
+router.patch("/acceptApplication/:appId", acceptApplication);
+
+router.get("/filter/:seekerId/:status", filterApplications);
 
 module.exports = router;
