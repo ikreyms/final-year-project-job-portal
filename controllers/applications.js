57098223742fc367b4c7da099d9559aa929d9b2e
Mohamed Ikram <ikreyms@gmail.com>
2022-05-14 02:49:25 +0500

    updated function: rejectApplications, acceptApplications

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,6 +1,8 @@
-const { application } = require("express");
 const Application = require("../models/Application");
+const Interview = require("../models/Interview");
+const Notification = require("../models/Notification");
 const responseToClient = require("../utils/responseToClient");
+const moment = require("moment");
 
 exports.createApplication = async (req, res, next) => {
   const { seekerId, jobId, empId } = req.params;
@@ -183,22 +185,35 @@ exports.getApplicationsReceived = async (req, res, next) => {
   }
 };
 
-exports.rejectApplication = async (req, res, next) => {
-  const { appId } = req.params;
+exports.rejectApplications = async (req, res, next) => {
+  const { selectionList } = req.body;
+  const { empId } = req.params;
   try {
-    const application = await Application.findOne({ _id: appId });
-    if (!application)
-      return responseToClient(res, 400, {
+    const applications = await Application.find({
+      _id: { $in: [...selectionList] },
+    });
+    if (applications.length === 0)
+      return responseToClient(res, 404, {
         success: false,
-        error: "Application not found",
+        error: "No applications found",
+        selectionList: [...selectionList],
       });
 
-    application.status = "Rejected";
-    await application.save();
+    await Application.updateMany(
+      { _id: { $in: [...selectionList] } },
+      { status: "Rejected" }
+    );
+
+    const newApplications = await Application.find({ empId, status: "Pending" })
+      .populate("jobId")
+      .populate("seekerId")
+      .populate("empId")
+      .sort({ createdAt: -1 });
 
     responseToClient(res, 200, {
       success: true,
-      message: "Application rejected.",
+      message: "Applications rejected.",
+      applications: newApplications,
     });
   } catch (error) {
     responseToClient(res, 500, {
@@ -209,29 +224,100 @@ exports.rejectApplication = async (req, res, next) => {
   }
 };
 
-exports.acceptApplication = async (req, res, next) => {
-  const { appId } = req.params;
+exports.acceptApplications = async (req, res, next) => {
+  const { venue, date, time, selectionList } = req.body;
+  const { empId } = req.params;
   try {
-    const application = await Application.findOne({ _id: appId });
-    if (!application)
-      return responseToClient(res, 400, {
+    const applications = await Application.find({
+      _id: { $in: [...selectionList] },
+    })
+      .populate({ path: "jobId", select: "title" })
+      .populate({ path: "empId", select: "companyName" });
+
+    if (applications.length === 0)
+      return responseToClient(res, 404, {
         success: false,
-        error: "Application not found",
+        error: "No applications found",
+        selectionList: [...selectionList],
       });
 
-    application.status = "Accepted";
-    await application.save();
+    await Application.updateMany(
+      { _id: { $in: [...selectionList] } },
+      { status: "Accepted" }
+    );
+
+    const newApplications = await Application.find({ empId, status: "Pending" })
+      .populate("jobId")
+      .populate("seekerId")
+      .populate("empId")
+      .sort({ createdAt: -1 });
+
+    // const x = await Application.aggregate([
+    //   { $group: { _id: "$jobId", seekerIds: { $push: { $seekerId } } } },
+    // ]);
+
+    // const seekerIds = await Application.find(
+    //   { _id: { $in: [...selectionList] } },
+    //   "seekerId"
+    // );
+    // const jobIds = await Application.find(
+    //   { _id: { $in: [...selectionList] } },
+    //   "jobId"
+    // );
+
+    await Interview.create({
+      empId,
+      appIds: [...selectionList],
+      venue,
+      interviewDate: date,
+      interviewTime: time,
+    });
+
+    applications.forEach(async (application) => {
+      await Notification.create({
+        receivers: [application.seekerId],
+        subject: "Application Accepted",
+        body: `Your job application for ${
+          application?.jobId.title
+        } was ACCEPTED by ${
+          application?.empId.companyName
+        }. Interview is scheduled to ${moment(date).format(
+          "DD/MM/YYYY"
+        )} | ${moment(time).format("HH:mm")}`,
+        postedBy: empId,
+      });
+    });
 
     responseToClient(res, 200, {
       success: true,
-      message: "Application accepted.",
+      message:
+        "Applications accepted. Interview created. Notifications created.",
+      applications: newApplications,
     });
   } catch (error) {
-    responseToClient(res, 500, {
-      success: false,
-      error: error.message,
-      errorFrom: "acceptApplication",
-    });
+    let errorMessage = {};
+
+    if (error.name === "ValidationError") {
+      Object.keys(error.errors).forEach((key) => {
+        errorMessage[key] = error.errors[key].message;
+      });
+      responseToClient(res, 400, {
+        success: false,
+        error: errorMessage,
+        errorFrom: "acceptApplication",
+      });
+    } else {
+      responseToClient(res, 500, {
+        success: false,
+        error: error.message,
+        errorFrom: "acceptApplication",
+      });
+    }
+    // responseToClient(res, 500, {
+    //   success: false,
+    //   error: error,
+    //   errorFrom: "acceptApplication",
+    // });
   }
 };
 
