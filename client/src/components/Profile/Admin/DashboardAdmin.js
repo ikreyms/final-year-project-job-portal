9e299704ac61117c7d82d4a8695f55be0f1293a3
Mohamed Ikram <ikreyms@gmail.com>
2022-05-17 18:25:20 +0500

    created

---
+++ client/src/components/Profile/Admin/DashboardAdmin.js
@@ -0,0 +1,66 @@
+import { Box, Divider, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import useStyles from "../styles";
+import StatPack from "../StatPack";
+import axios from "axios";
+
+const DashboardAdmin = () => {
+  const classes = useStyles();
+
+  const [profileData, setProfileData] = useState({});
+
+  const getProfileData = async () => {
+    try {
+      const response = await axios.get("http://localhost:2900/api/admin");
+      setProfileData(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    getProfileData();
+  }, []);
+
+  return (
+    <Box className={classes.panelWrapper}>
+      <Typography variant="h5" mb={2}>
+        Admin Dashboard
+      </Typography>
+
+      <Divider sx={{ mt: 1, mb: 3 }} />
+
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: 2,
+          justifyContent: "space-around",
+        }}
+      >
+        <StatPack
+          stat={profileData.seekerCount}
+          caption="Seekers Registered"
+          color="primary"
+        />
+        <StatPack
+          stat={profileData.employerCount}
+          caption="Employers Registered"
+          color="primary"
+        />
+        <StatPack
+          stat={profileData.jobCount}
+          caption="Total Jobs Posted"
+          color="primary"
+        />
+        <StatPack
+          stat={profileData.applicationCount}
+          caption="Applications"
+          color="primary"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default DashboardAdmin;
