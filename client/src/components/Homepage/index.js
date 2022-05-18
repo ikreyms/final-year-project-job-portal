d79229dfcae9f1b35af4810cecf9e9696f43c628
Mohamed Ikram <ikreyms@gmail.com>
2022-05-18 11:46:58 +0500

    enabled loading of homepage data

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SectionOne from "./SectionOne";
 import SectionTwo from "./SectionTwo";
 import SectionThree from "./SectionThree";
 import SectionFour from "./SectionFour";
+import axios from "axios";
 
 const Homepage = () => {
+  const [homepageData, setHomepageData] = useState();
+
+  const loadData = async () => {
+    try {
+      const response = await axios.get("http://localhost:2900/api/admin");
+      setHomepageData(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div>
       <SectionOne />
       <SectionTwo />
-      <SectionThree />
+      <SectionThree homepageData={homepageData} />
       <SectionFour />
     </div>
   );
