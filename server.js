ada936b79ec558c60f97fe7e3509adeab0b5da10
Mohamed Ikram <ikreyms@gmail.com>
2022-07-08 14:45:34 +0500

    1.0.0

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const cors = require("cors");
 const express = require("express");
 const authRoutes = require("./routes/auth");
@@ -32,6 +33,16 @@ app.use("/api/applications", applicationRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/interviews", interviewRoutes);
 app.use("/api/admin", adminRoutes);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 2900, () =>
   console.log(`Server running on port ${process.env.PORT}`)
